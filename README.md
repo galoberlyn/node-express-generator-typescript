a018061833fc690db9c328bc207cccf5127abedf
Galo Berlyn Garlejo <galo@penbrothers.com>
2021-04-22 21:09:04 +0800

    add: boilerplate files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-express-generator-typescript
 A TypeScript version of express-generator --no-view
+
+## Motivation
+To have a fresh boilerplate of express-generator using TypeScript
+
+Easy to use and start your API server with express js
+
+
+© Galo Garlejo galo@penbrothers.com
---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var app = express_1.default();
+app.use(logger('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+module.exports = app;
